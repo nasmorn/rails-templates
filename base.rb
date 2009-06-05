f9d26736ef24ac0e94f9e490b64b18a6fb1541b0
nasmorn <neweconomist@me.com>
2009-06-05 20:27:17 +0200

    Exception logger working

--- base.rb
+++ base.rb
@@ -1,11 +1,27 @@
 # This is what I want to do in any rails app
 run "echo TODO > README"
 
-gem 'exception_notification'
-rake "gems:install"
-
 git :init
-
 run "cp config/database.yml config/example_database.yml"
 
-git :add => ".", :commit => "-m 'initial commit"
+git :add => ".", :commit => "-m 'initial commit'"
+
+# Here we add the exception logger so that no exception ever excapes us again
+git :clone => 'git://github.com/defunkt/exception_logger.git vendor/plugins/exception_logger'
+generate :exception_migration
+rake "db:migrate"
+
+file 'app/controllers/application_controller.rb', 
+%q{class ApplicationController < ActionController::Base
+ 
+  helper :all
+ 
+  protect_from_forgery
+ 
+  include ExceptionLoggable
+  filter_parameter_logging :password, :password_confirmation
+end
+}
+
+route "map.connect 'logged_exceptions/:action/:id', :controller => 'logged_exceptions'"
+git :add => ".", :commit => "-m 'Added the exception logger'"
